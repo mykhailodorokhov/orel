4573635f0c60c0dcf4d0ebb6d9fef005ca665532
Mykhailo Dorokhov <michaeldorokhov@gmail.com>
2019-09-12 02:13:23 +0300

    added and tuned docker-compose

---
+++ scripts/docker-compose-scripts.sh
@@ -0,0 +1,5 @@
+# To start composing
+docker-compose up
+
+# To set-up and migrate database
+docker exec -it orel-app npm run db-setup-docker
--- scripts/docker-scripts.sh
+++ scripts/docker-scripts.sh
@@ -2,17 +2,18 @@
 docker build . -t orel
 
 # To run the application docker image
-docker run --rm -d --name=orel-app -e PORT=3000 -e DB_USERNAME=root -e DB_PASSWORD=password -e DB_NAME=orel-docker -e DB_HOSTNAME=172.17.0.1 -p 3001:3000 orel
+docker run --rm -d --name=orel-app -e PORT=3000 -e DB_USERNAME=root -e DB_PASSWORD=password -e DB_NAME=orel-docker -e DB_HOSTNAME=172.17.0.1 -p 3000:3000 orel
 
 # To start a MySQL docker container
 docker run --rm -d --name=orel-mysql -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 mysql:5.7.27
 
-# To look for the actual DB_HOSTNAME address
-docker details orel-mysql
+# To look for the actual DB_HOSTNAME address (just to be sure, but it's usually 172.17.0.1)
+docker inspect orel-mysql
 
-# To set up and migrate database
-docker exec -it orel-app npm run db-setup
+# To set up and migrate database (choose the needed one)
+docker exec -it orel-app npm run db-setup-docker
 
+# NB! Only applyable if you want to work with MySQL 8.x or higher.
 # (fix to this issue https://github.com/sidorares/node-mysql2/issues/991)
 # Re-setting the password will solve the issue if on MySQL 8.x and higher you are getting the following error :
 # "ERROR: Client does not support authentication protocol requested by server; consider upgrading MySQL client"
